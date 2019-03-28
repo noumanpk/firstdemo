ae415f13e0148882acb23272c1c43f9b11bd88cd
noumanpk <44252608+noumanpk@users.noreply.github.com>
2019-03-28 12:55:39 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -5,7 +5,7 @@ public class HelloWorld {
   public static void main(String[] args) {
     System.out.println("Hello, World");
 	    System.out.println("Welcome to Capgemini");
-System.out.println("This was developed by Nouman");
+System.out.println("This was developed by developer");
 
   }
 }
