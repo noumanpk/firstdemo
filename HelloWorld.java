e9405c3b3bedfaf6e17eb30e5a7b9e5c96b78d87
noumanpk <44252608+noumanpk@users.noreply.github.com>
2019-03-28 13:52:25 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -6,7 +6,7 @@ public class HelloWorld
   public static void main(String[] args) {
     	System.out.println("Hello, World");
 	System.out.println("Welcome to Capgemini");
- 	System.out.println("Hello, World"10+20);
+ 	System.out.println("Hello, World"10+20+30);
 	  
 
 
